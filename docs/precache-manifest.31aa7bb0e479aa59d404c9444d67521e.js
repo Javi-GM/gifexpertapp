dec55eab848cfc888a9dd1428ffe5145bc440d8f
JaviGM <javi46018@gmail.com>
2020-10-06 14:38:01 +0200

    rename to docs

---
+++ docs/precache-manifest.31aa7bb0e479aa59d404c9444d67521e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845927c99acfcd035bda4c515775e9d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d47176f6bace89a91d6",
+    "url": "/static/css/main.363f61ec.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d47176f6bace89a91d6",
+    "url": "/static/js/main.4eb50037.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
